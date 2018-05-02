2c7aee5b01452525ff7bb77aacb5eefa9c256b53
rmcintosh <rmcintosh@users.noreply.github.com>
2018-05-02 15:19:53 -0400

    simplify

--- blueprints.py
+++ blueprints.py
@@ -9,38 +9,30 @@ api = Blueprint("api", "api", url_prefix="/api")
 
 @api.route("/")
 def all():
-    status_json = status_json_or_404()
-
-    return status_json
+    return status_json_or_404()
 
 
 @api.route("/info/")
 @api.route("/info/<field>/")
 def info(field=None):
-    status_json = status_json_or_404(endpoint="info",
-                                     field=field)
-
-    return status_json
+    return status_json_or_404(endpoint="info",
+                              field=field)
 
 
 @api.route("/programstatus/")
 @api.route("/programstatus/<field>/")
 def programstatus(field=None):
-    status_json = status_json_or_404(endpoint="programstatus",
-                                     field=field)
-
-    return status_json
+    return status_json_or_404(endpoint="programstatus",
+                              field=field)
 
 
 @api.route("/hoststatus/")
 @api.route("/hoststatus/<host_name>/")
 @api.route("/hoststatus/<host_name>/<field>/")
 def hoststatus(host_name=None, field=None):
-    status_json = status_json_or_404(endpoint="hoststatus",
-                                     host_name=host_name,
-                                     field=field)
-
-    return status_json
+    return status_json_or_404(endpoint="hoststatus",
+                              host_name=host_name,
+                              field=field)
 
 
 @api.route("/servicestatus/")
@@ -48,31 +40,25 @@ def hoststatus(host_name=None, field=None):
 @api.route("/servicestatus/<host_name>/<service_description>/")
 @api.route("/servicestatus/<host_name>/<service_description>/<field>/")
 def servicestatus(host_name=None, service_description=None, field=None):
-    status_json = status_json_or_404(endpoint="servicestatus",
-                                     host_name=host_name,
-                                     service_description=service_description,
-                                     field=field)
-
-    return status_json
+    return status_json_or_404(endpoint="servicestatus",
+                              host_name=host_name,
+                              service_description=service_description,
+                              field=field)
 
 
 @api.route("/contactstatus/")
 @api.route("/contactstatus/<contact_name>/")
 @api.route("/contactstatus/<contact_name>/<field>/")
 def contactstatus(contact_name=None, field=None):
-    status_json = status_json_or_404(endpoint="contactstatus",
-                                     contact_name=contact_name,
-                                     field=field)
-
-    return status_json
+    return status_json_or_404(endpoint="contactstatus",
+                              contact_name=contact_name,
+                              field=field)
 
 
 @api.route("/hostcomment/")
 @api.route("/hostcomment/<host_name>/")
 @api.route("/hostcomment/<host_name>/<field>/")
 def hostcomment(host_name=None, field=None):
-    status_json = status_json_or_404(endpoint="hostcomment",
-                                     host_name=host_name,
-                                     field=field)
-
-    return status_json
+    return status_json_or_404(endpoint="hostcomment",
+                              host_name=host_name,
+                              field=field)
